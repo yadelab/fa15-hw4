738064844e9bf3068e66f6206fea0799707d2d90
Yadel <yabraham@berkeley.edu>
2015-10-10 19:21:34 -0700

    Im done

---
+++ test/controllers/dog_controller_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class DogControllerControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/helpers/dog_controller_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class DogControllerHelperTest < ActionView::TestCase
+end
